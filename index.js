51b5da266c92ffc06a4888a1b4b761d445df2c93
AngelFmc <612110393@utsalamanca.edu.mx>
2022-10-19 12:32:18 -0500

    Conexión a BD

--- index.js
+++ index.js
@@ -16,18 +16,34 @@ app.use(bodyparser.json())
 
 //Conexion a la Base de datos
 
+const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.jxeyr0p.mongodb.net/${process.env.DBNAME}test`
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Conectado a BD')
+}).catch(e =>{
+    console.log('error: ', e)
+})
+
 
 //Importar rutas
+const authRoutes = require('./routes/auth')
+
 
 //Ruta del Middleware
-app.get('/', (req,res) =>{
+app.use('/api/user', authRoutes)
+
+
+app.get('/', (req, res) =>{
     res.json({
         estado: true,
         mensaje: 'WORKS FINE!!!'
     })
 })
 
-//Inivializar servidor
+//Inicializar servidor
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () =>{
     console.log(`Servidor Corriendo: ${PORT}`)
